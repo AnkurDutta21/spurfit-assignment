9902004f45c07273786dd455af23ef69af753507
AnkurDutta21 <emxax54@gmail.com>
2024-07-18 14:16:03 +0530

    readme update

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Workout Builder Assignment
+# Spur.fit Assignment
 
 This project is a React-based workout builder application where users can drag and drop different workout blocks to create a custom workout. The application is styled using Tailwind CSS and uses `react-beautiful-dnd` for drag-and-drop functionality.
 
@@ -7,13 +7,18 @@ This project is a React-based workout builder application where users can drag a
 - **Drag and Drop**: Add workout blocks to the graph by dragging them from the list.
 - **Dynamic Rendering**: The graph updates dynamically based on the blocks added.
 - **Clear Blocks**: Reset the graph with a "Clear Blocks" button.
-- **Styled with Tailwind CSS**: Modern and responsive design.
+- **Styled with Tailwind CSS**: For modern design.
+- **ToolTip with react-tooltip**: For user friendly interactions
 
+## Demo
+ https://github.com/user-attachments/assets/0692d28a-8e3c-4935-9b6d-1a466f8503e5
+
+  
 ## Installation
 
 1. Clone the repository:
     ```sh
-    git clone https://github.com/your-username/workout-builder.git
+    git clone https://github.com/AnkurDutta21/spurfit-assignment.git
     cd workout-builder
     ```
 
@@ -26,3 +31,15 @@ This project is a React-based workout builder application where users can drag a
     ```sh
     npm start
     ```
+## Usage
+
+1. Open your browser and navigate to `http://localhost:3000` to see the application in action.
+2. Drag and drop blocks from the "Add Workout" section to the "Graph" section to build your workout.
+3. Click the "Clear Blocks" button to reset the graph and start over.
+
+## Components
+
+- **Topbar**: Displays the page title and a button for additional actions.
+- **AddWorkout**: Contains draggable workout blocks that can be added to the graph.
+- **Graph**: Visual representation of the workout blocks, supports drag-and-drop functionality.
+- **Steps**: Displays the added blocks and allows further customization.
--- src/pages/WorkoutPage.jsx
+++ src/pages/WorkoutPage.jsx
@@ -3,24 +3,24 @@ import Graph from '../components/workoutPage/Graph';
 import AddWorkout from '../components/workoutPage/AddWorkout';
 import Topbar from '../components/common/Topbar';
 import Steps from '../components/workoutPage/Steps';
-import block0 from '../assets/Block.png';
-import block1 from '../assets/Block (1).png';
-import block2 from '../assets/Block (2).png';
-import block3 from '../assets/Block (3).png';
-import block4 from '../assets/Block (4).png';
-import block5 from '../assets/Block (5).png';
+import warmUp from '../assets/warmUp.png';
+import active from '../assets/active.png';
+import cooldown from '../assets/cooldown.png';
+import repeatSteps from '../assets/repeatSteps.png';
+import rampUp from '../assets/rampUp.png';
+import rampDown from '../assets/rampDown.png';
 import { DragDropContext } from 'react-beautiful-dnd';
 
 const WorkoutPage = () => {
   const [blocks, setBlocks] = useState([]);
 
   const initialBlocks = [
-    { id: '1', type: 'Warm Up', distance: 3, image: block0 },
-    { id: '2', type: 'Active', distance: 3, image: block1 },
-    { id: '3', type: 'Cool Down', distance: 3, image: block2 },
-    { id: '4', type: 'Repeat Steps', distance: 4, subSteps: ['2 KM', '2 KM'], image: block3 },
-    { id: '5', type: 'Ramp Up', distance: 5, subSteps: ['2 KM', '1 KM', '1 KM', '1 KM'], image: block4 },
-    { id: '6', type: 'Ramp Down', distance: 4, subSteps: ['1 KM', '1 KM', '1 KM', '1 KM'], image: block5 },
+    { id: '1', type: 'Warm Up', distance: 3, image: warmUp },
+    { id: '2', type: 'Active', distance: 3, image: active },
+    { id: '3', type: 'Cool Down', distance: 3, image: cooldown },
+    { id: '4', type: 'Repeat Steps', distance: 4, subSteps: ['2 KM', '2 KM'], image: repeatSteps },
+    { id: '5', type: 'Ramp Up', distance: 5, subSteps: ['2 KM', '1 KM', '1 KM', '1 KM'], image: rampUp },
+    { id: '6', type: 'Ramp Down', distance: 4, subSteps: ['1 KM', '1 KM', '1 KM', '1 KM'], image: rampDown },
   ];
 
   const handleBtnClick = () => {
